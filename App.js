2168708d986f71efa37634c61f7d51bcf95c300b
Aniket Pandharabale <aniketicloud@gmail.com>
2022-04-29 00:56:31 +0530

    exploring layout and flexbox

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello React Native World . . .!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View>
+        <TextInput placeholder='Type Your goal' />
+        <Button title='Add Goal' />
+      </View>
+      <View>
+        <Text>
+          List of goals . . .
+        </Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appContainer: {
+    padding: 50
   },
 });
