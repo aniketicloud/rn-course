56d22828a0aec9cece29127003d2184d494bf61a
Aniket Pandharabale <aniketicloud@gmail.com>
2022-04-29 22:45:38 +0530

    improving the layout

--- App.js
+++ App.js
@@ -1,37 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
-import { FlexboxDeep } from './FlexboxDeep';
 
 export default function App() {
   return (
-    // <View style={styles.appContainer}>
-    //   <View style={styles.inputContainer}>
-    //     <TextInput placeholder='Type Your goal' style={styles.textInput} />
-    //     <Button title='Add Goal' />
-    //   </View>
-    //   <View>
-    //     <Text>
-    //       List of Goals . . Coming Soon . . .!
-    //     </Text>
-    //   </View>
-    // </View>
-      <FlexboxDeep />
+    <View style={styles.appContainer}>
+
+      <View style={styles.inputContainer}>
+        <TextInput placeholder='Type Your goal' style={styles.textInput} />
+        <Button title='Add Goal' />
+      </View>
+
+      <View style={styles.goalsContainer} >
+        <Text>
+          List of Goals . . Coming Soon . . .!
+        </Text>
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   appContainer: {
-    // paddingTop: 50
+    // !! Delete this
+    backgroundColor: 'grey',
+
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
   },
   inputContainer: {
+    // !! Delete this
+    backgroundColor: 'white',
+
+    flex: 1,
     flexDirection: 'row',
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc'
   },
   textInput: {
     borderWidth: 1,
     borderColor: "#ccc",
-    width: '80%',
+    width: '70%',
     marginRight: 8,
-    padding: 8
+    padding: 8,
+  },
+  goalsContainer: {
+    // !! Delete this
+    backgroundColor: 'aqua',
+    marginBottom: 24,
+
+    flex: 5,
   }
 });
